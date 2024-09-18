e657e33ec51086cce24f821f406315cd1d10dd86
EnricCostaSierra <152902101+EnricCostaSierra@users.noreply.github.com>
2024-09-18 17:27:00 +0200

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
  public class helloWorld {
  
  public static void main(String[] args) {
-        System.out.println("Hello world 24-25\n--------------------------------");
+        System.out.println("Hello world 24-25/n--------------------------------");
         
     }
  }
