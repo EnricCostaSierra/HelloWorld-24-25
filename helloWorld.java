6e5fba88629e48d0195a632149e2d6818a099c45
EnricCostaSierra <adam23ecosta@inslaferreria.cat>
2024-09-18 17:23:09 +0200

    subrallat

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
  public class helloWorld {
  
  public static void main(String[] args) {
-        System.out.println("Hello world 24-25");
+        System.out.println("Hello world 24-25\n--------------------------------");
         
     }
  }
